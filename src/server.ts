3eb7729816f1a1784efa63ca947a2b728ada5a05
CollyneJumah <collynejumah2010@gmail.com>
2022-07-26 12:15:33 +0300

    added get filteredimage endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {Router, Request,Response} from 'express'
 
 (async () => {
 
@@ -30,7 +31,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage', async (req: Request, res: Response) =>{
+    const { image_url } = req.query.image_url.toString()
+
+    if(!image_url)
+    {
+      res.status(400).send("Image URL is required")
+    }
+
+    const filtered_image = await filterImageFromURL(image_url)
+    res.status(200).sendFile(filtered_image, ()=>{
+      deleteLocalFiles([filtered_image])
+    })
+  })
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
